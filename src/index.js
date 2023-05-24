f07a46e6714383d6ad26651a27712d4690e838c5
Ernest-Kurish <kurish.ernest@gmail.com>
2023-05-24 12:35:57 +0900

    updated

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { App } from './components/App';
-// import './index.css';
-
-// const rootElement = document.getElementById('root');
-// ReactDOM.createRoot(rootElement).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
 const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
+const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
